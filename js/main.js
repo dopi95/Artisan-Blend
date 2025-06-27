8fa7438ba218fae51fe3f71b8ad531d56349855a
Ishimwe Eric <ishimwee299@gmail.com>
2025-06-27 12:04:29 +0200

    Solved initial merge conficts

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function showMessage() {
-  const message = document.getElementById("message");
-  message.classList.toggle("hidden"); 
-  if (!message.classList.contains("hidden")) {
-    message.textContent = "JavaScript is working! ✅";
-  }
-}
